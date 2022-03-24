ed87ea1f566339aec2d324cdb3570a86db0e4d08
Luis Gustavo <luisduarte.gr348@academico.ifsul.edu.br>
2022-03-24 11:41:04 -0300

    Criado os metodos Constructor, Getters e Setters e toString na classe Pizza

--- pvtjcombobox/Pizza.java
+++ pvtjcombobox/Pizza.java
@@ -10,6 +10,33 @@ package pvtjcombobox;
  * @author thais
  */
 public class Pizza {
-    private String pizza;
+    private String sabor;
     private double valor;
+
+    public Pizza(String sabor, double valor) {
+        this.sabor = sabor;
+        this.valor = valor;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return sabor;
+    }
+    
 }
